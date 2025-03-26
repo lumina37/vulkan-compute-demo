153414e3f00a0653daf10ebe41811734cd61c4f0
lumina37 <starry.qvq@gmail.com>
2025-03-26 20:04:44 +0800

    perf: simplify shader

--- shader/hlsl/gaussFilterV1.hlsl
+++ shader/hlsl/gaussFilterV1.hlsl
@@ -12,20 +12,25 @@ struct UBO {
 };
 [[vk::binding(3)]] ConstantBuffer<UBO> ubo;
 
-float4 blurAtIdx(Texture2D tex, int2 idx) {
+[numthreads(16, 16, 1)] void main(uint3 dtid : SV_DispatchThreadID) {
+    int2 dstIdx = int2(dtid.xy);
+    int2 dstSize;
+    dstImage.GetDimensions(dstSize.x, dstSize.y);
+    if (dstIdx.x >= dstSize.x || dstIdx.y >= dstSize.y) {
+        return;
+    }
+
     int kSize = pc.kernelSize;
     int halfKSize = kSize / 2;
-    int2 srcSize;
-    tex.GetDimensions(srcSize.x, srcSize.y);
 
     float4 color = {0.0, 0.0, 0.0, 0.0};
     for (int y = -halfKSize; y <= halfKSize; y++) {
         float4 rowAcc = {0.0, 0.0, 0.0, 0.0};
 
         for (int x = -halfKSize; x <= halfKSize; x++) {
-            int2 inCoord = idx + int2(x, y);
-            float2 uv = (float2(inCoord) + 0.5) / float2(srcSize);
-            float4 srcVal = tex.SampleLevel(srcSampler, uv, 0);
+            int2 inCoord = dstIdx + int2(x, y);
+            float2 uv = (float2(inCoord) + 0.5) / float2(dstSize);
+            float4 srcVal = srcTex.SampleLevel(srcSampler, uv, 0);
 
             int absX = abs(x);
             int weightY = absX >> 2;
@@ -41,19 +46,5 @@ float4 blurAtIdx(Texture2D tex, int2 idx) {
         color = mad(rowAcc, weight, color);
     }
 
-    color.w = 1.0;
-
-    return color;
-}
-
-[numthreads(16, 16, 1)] void main(uint3 dtid : SV_DispatchThreadID) {
-    int2 dstIdx = int2(dtid.xy);
-    int2 dstSize;
-    dstImage.GetDimensions(dstSize.x, dstSize.y);
-    if (dstIdx.x >= dstSize.x || dstIdx.y >= dstSize.y) {
-        return;
-    }
-
-    float4 color = blurAtIdx(srcTex, dstIdx);
-    dstImage[dstIdx] = color;
+    dstImage[dstIdx] = float4(color.xyz, 1.0);
 }
