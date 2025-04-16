f75f194e575902029d9a63362af0d474c33b036f
lumina37 <starry.qvq@gmail.com>
2025-04-16 18:49:47 +0800

    refactor: migrate V2 back to GLSL

--- shader/spirv/gaussFilter.hpp
+++ shader/spirv/gaussFilter.hpp
@@ -19,7 +19,7 @@ namespace _spirv::gaussFilterV1 {
 
 namespace _spirv::gaussFilterV2 {
 
-#include "spirv/gaussFilterV2.hlsl.h"
+#include "spirv/gaussFilterV2.h"
 
 }
 
@@ -28,7 +28,7 @@ static const std::span gaussFilterV0SpirvCode{(std::byte*)_spirv::gaussFilterV0:
                                               sizeof(_spirv::gaussFilterV0::spirvCode)};
 static const std::span gaussFilterV1SpirvCode{(std::byte*)_spirv::gaussFilterV1::spirvCode,
                                               sizeof(_spirv::gaussFilterV1::spirvCode)};
-static const std::span gaussFilterV2SpirvCode{(std::byte*)_spirv::gaussFilterV2::g_main,
-                                              sizeof(_spirv::gaussFilterV2::g_main)};
+static const std::span gaussFilterV2SpirvCode{(std::byte*)_spirv::gaussFilterV2::spirvCode,
+                                              sizeof(_spirv::gaussFilterV2::spirvCode)};
 
 }  // namespace shader
