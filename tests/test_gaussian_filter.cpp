979e0f2800a33c62568e01ed087b7fc955d55c53
lumina37 <starry.qvq@gmail.com>
2025-04-29 18:43:54 +0800

    chore: update tests

--- tests/test_gaussian_filter.cpp
+++ tests/test_gaussian_filter.cpp
@@ -1,8 +1,11 @@
 #include <cmath>
 #include <cstddef>
 #include <cstdint>
+#include <expected>
+#include <filesystem>
 #include <iostream>
 #include <limits>
+#include <print>
 #include <random>
 #include <ranges>
 #include <span>
@@ -12,7 +15,27 @@
 #include "spirv/gaussFilter.hpp"
 #include "vkc.hpp"
 
+namespace fs = std::filesystem;
 namespace rgs = std::ranges;
+
+class Unwrap {
+public:
+    template <typename T>
+    static friend auto operator|(std::expected<T, vkc::Error>&& src, [[maybe_unused]] const Unwrap& _) {
+        if (!src.has_value()) {
+            const auto& err = src.error();
+            const fs::path filePath{err.source.file_name()};
+            const std::string fileName = filePath.filename().string();
+            std::println(std::cerr, "{}:{} msg={} code={}", fileName, err.source.line(), err.msg, err.code);
+            std::exit(err.code);
+        }
+        if constexpr (!std::is_void_v<T>) {
+            return std::forward_like<T>(src.value());
+        }
+    }
+};
+
+constexpr auto unwrap = Unwrap();
 
 void gaussianFilterRefImpl(const std::span<const std::byte> src, const std::span<std::byte> dst,
                            const vkc::Extent extent, const int kernelSize, const float sigma) {
@@ -72,7 +95,7 @@ TEST_CASE("Gaussian Blur", "hlsl::gaussFilterVx") {
     constexpr vkc::Extent extent{256, 256, vk::Format::eR8G8B8A8Unorm};
 
     // Src data
-    vkc::StbImageManager srcImage{extent};
+    vkc::StbImageManager srcImage = vkc::StbImageManager::createWithExtent(extent) | unwrap;
     std::default_random_engine rdEngine;
     rdEngine.seed(37);
     for (auto& val : srcImage.getImageSpan()) {
@@ -80,51 +103,62 @@ TEST_CASE("Gaussian Blur", "hlsl::gaussFilterVx") {
     }
 
     // CPU Reference
-    vkc::StbImageManager dstImageCpuRef{srcImage.getExtent()};
+    vkc::StbImageManager dstImageCpuRef = vkc::StbImageManager::createWithExtent(srcImage.getExtent()) | unwrap;
     gaussianFilterRefImpl(srcImage.getImageSpan(), dstImageCpuRef.getImageSpan(), srcImage.getExtent(), kernelSize,
                           sigma);
-    dstImageCpuRef.saveTo("ref.png");
+    dstImageCpuRef.saveTo("ref.png") | unwrap;
 
-    vkc::StbImageManager dstImageVk{srcImage.getExtent()};
+    vkc::StbImageManager dstImageVk = vkc::StbImageManager::createWithExtent(srcImage.getExtent()) | unwrap;
 
     // Device
-    vkc::InstanceManager instMgr;
-    vkc::PhysicalDeviceManager phyDeviceMgr{instMgr};
-    const uint32_t computeQFamilyIdx = defaultComputeQFamilyIndex(phyDeviceMgr);
-    auto pDeviceMgr = std::make_shared<vkc::DeviceManager>(phyDeviceMgr, computeQFamilyIdx);
-    vkc::QueueManager queueMgr{*pDeviceMgr, computeQFamilyIdx};
+    vkc::InstanceManager instMgr = vkc::InstanceManager::create() | unwrap;
+    vkc::PhysicalDeviceManager phyDeviceMgr = vkc::PhysicalDeviceManager::create(instMgr) | unwrap;
+    const uint32_t computeQFamilyIdx = defaultComputeQFamilyIndex(phyDeviceMgr) | unwrap;
+    auto pDeviceMgr =
+        std::make_shared<vkc::DeviceManager>(vkc::DeviceManager::create(phyDeviceMgr, computeQFamilyIdx) | unwrap);
+    vkc::QueueManager queueMgr = vkc::QueueManager::create(*pDeviceMgr, computeQFamilyIdx) | unwrap;
 
     // Descriptor & Layouts
-    vkc::SamplerManager samplerMgr{pDeviceMgr};
+    vkc::SamplerManager samplerMgr = vkc::SamplerManager::create(pDeviceMgr) | unwrap;
     vkc::PushConstantManager kernelSizePcMgr{std::pair{kernelSize, sigma * sigma * 2.0f}};
-    vkc::ImageManager srcImageMgr{phyDeviceMgr, pDeviceMgr, srcImage.getExtent(), vkc::ImageType::Read};
+    vkc::ImageManager srcImageMgr =
+        vkc::ImageManager::create(phyDeviceMgr, pDeviceMgr, srcImage.getExtent(), vkc::ImageType::Read) | unwrap;
     const std::array srcImageMgrCRefs{std::cref(srcImageMgr)};
-    vkc::ImageManager dstImageMgr{phyDeviceMgr, pDeviceMgr, srcImage.getExtent(), vkc::ImageType::Write};
+    vkc::ImageManager dstImageMgr =
+        vkc::ImageManager::create(phyDeviceMgr, pDeviceMgr, srcImage.getExtent(), vkc::ImageType::Write) | unwrap;
     const std::array dstImageMgrCRefs{std::cref(dstImageMgr)};
-    srcImageMgr.uploadFrom(srcImage.getImageSpan());
+    srcImageMgr.uploadFrom(srcImage.getImageSpan()) | unwrap;
 
     const std::vector descPoolSizes = genPoolSizes(srcImageMgr, samplerMgr, dstImageMgr);
-    vkc::DescPoolManager descPoolMgr{pDeviceMgr, descPoolSizes};
+    vkc::DescPoolManager descPoolMgr = vkc::DescPoolManager::create(pDeviceMgr, descPoolSizes) | unwrap;
 
     const std::array gaussDLayoutBindings = genDescSetLayoutBindings(srcImageMgr, samplerMgr, dstImageMgr);
-    vkc::DescSetLayoutManager gaussDLayoutMgr{pDeviceMgr, gaussDLayoutBindings};
-    const std::array gaussDLayoutMgrs{std::cref(gaussDLayoutMgr)};
-    vkc::PipelineLayoutManager gaussPLayoutMgr{pDeviceMgr, gaussDLayoutMgrs, kernelSizePcMgr.getPushConstantRange()};
-    vkc::DescSetsManager gaussDescSetsMgr{pDeviceMgr, descPoolMgr, gaussDLayoutMgrs};
+    vkc::DescSetLayoutManager gaussDLayoutMgr =
+        vkc::DescSetLayoutManager::create(pDeviceMgr, gaussDLayoutBindings) | unwrap;
+    const std::array gaussDLayoutMgrCRefs{std::cref(gaussDLayoutMgr)};
+    vkc::PipelineLayoutManager gaussPLayoutMgr =
+        vkc::PipelineLayoutManager::createWithPushConstant(pDeviceMgr, gaussDLayoutMgrCRefs,
+                                                           kernelSizePcMgr.getPushConstantRange()) |
+        unwrap;
+    vkc::DescSetsManager gaussDescSetsMgr =
+        vkc::DescSetsManager::create(pDeviceMgr, descPoolMgr, gaussDLayoutMgrCRefs) | unwrap;
     const std::array gaussWriteDescSets = genWriteDescSets(srcImageMgr, samplerMgr, dstImageMgr);
     const std::array gaussWriteDescSetss{std::span{gaussWriteDescSets.begin(), gaussWriteDescSets.end()}};
     gaussDescSetsMgr.updateDescSets(gaussWriteDescSetss);
 
     // Command Buffer
-    auto pCommandPoolMgr = std::make_shared<vkc::CommandPoolManager>(pDeviceMgr, computeQFamilyIdx);
-    vkc::CommandBufferManager gaussCmdBufMgr{pDeviceMgr, pCommandPoolMgr};
+    auto pCommandPoolMgr = std::make_shared<vkc::CommandPoolManager>(
+        vkc::CommandPoolManager::create(pDeviceMgr, computeQFamilyIdx) | unwrap);
+    vkc::CommandBufferManager gaussCmdBufMgr = vkc::CommandBufferManager::create(pDeviceMgr, pCommandPoolMgr) | unwrap;
 
     SECTION("v0") {
         constexpr vkc::BlockSize blockSize{16, 16, 1};
-        vkc::ShaderManager gaussShaderMgr{pDeviceMgr, shader::gaussFilterV0SpirvCode};
+        vkc::ShaderManager gaussShaderMgr =
+            vkc::ShaderManager::create(pDeviceMgr, shader::gaussFilterV0SpirvCode) | unwrap;
         vkc::SpecConstantManager specConstantMgr{blockSize.x, blockSize.y};
-        vkc::PipelineManager gaussPipelineMgr{pDeviceMgr, gaussPLayoutMgr, gaussShaderMgr,
-                                              specConstantMgr.getSpecInfo()};
+        vkc::PipelineManager gaussPipelineMgr =
+            vkc::PipelineManager::create(pDeviceMgr, gaussPLayoutMgr, gaussShaderMgr, specConstantMgr.getSpecInfo()) |
+            unwrap;
 
         gaussCmdBufMgr.begin();
         gaussCmdBufMgr.bindPipeline(gaussPipelineMgr);
@@ -158,51 +192,14 @@ TEST_CASE("Gaussian Blur", "hlsl::gaussFilterVx") {
         REQUIRE(avgDiff < maxValidAvgDiff);
     }
 
-    SECTION("v1") {
-        constexpr vkc::BlockSize blockSize{256, 1, 1};
-        vkc::ShaderManager gaussShaderMgr{pDeviceMgr, shader::gaussFilterV1SpirvCode};
-        vkc::SpecConstantManager specConstantMgr{blockSize.x};
-        vkc::PipelineManager gaussPipelineMgr{pDeviceMgr, gaussPLayoutMgr, gaussShaderMgr,
-                                              specConstantMgr.getSpecInfo()};
-
-        gaussCmdBufMgr.begin();
-        gaussCmdBufMgr.bindPipeline(gaussPipelineMgr);
-        gaussCmdBufMgr.bindDescSets(gaussDescSetsMgr, gaussPLayoutMgr);
-        gaussCmdBufMgr.pushConstant(kernelSizePcMgr, gaussPLayoutMgr);
-        gaussCmdBufMgr.recordSrcPrepareTranfer(srcImageMgrCRefs);
-        gaussCmdBufMgr.recordUploadToSrc(srcImageMgrCRefs);
-        gaussCmdBufMgr.recordSrcPrepareShaderRead(srcImageMgrCRefs);
-        gaussCmdBufMgr.recordDstPrepareShaderWrite(dstImageMgrCRefs);
-        gaussCmdBufMgr.recordDispatch(srcImage.getExtent(), blockSize);
-        gaussCmdBufMgr.recordDstPrepareTransfer(dstImageMgrCRefs);
-        gaussCmdBufMgr.recordDownloadToDst(dstImageMgrCRefs);
-        gaussCmdBufMgr.recordWaitDownloadComplete(dstImageMgrCRefs);
-        gaussCmdBufMgr.end();
-
-        gaussCmdBufMgr.submitTo(queueMgr);
-        gaussCmdBufMgr.waitFence();
-
-        dstImageMgr.downloadTo(dstImageVk.getImageSpan());
-        dstImageVk.saveTo("v1.png");
-
-        int diffAcc = 0;
-        for (const auto [lhs, rhs] : rgs::views::zip(dstImageCpuRef.getImageSpan(), dstImageVk.getImageSpan())) {
-            int diff = std::abs((int)lhs - (int)rhs);
-            REQUIRE(diff <= maxValidDiff);
-            diffAcc += diff;
-        }
-        float avgDiff =
-            (float)diffAcc / (float)dstImageVk.getExtent().size() / (float)std::numeric_limits<uint8_t>::max();
-
-        REQUIRE(avgDiff < maxValidAvgDiff);
-    }
-
     SECTION("v2") {
         constexpr vkc::BlockSize blockSize{256, 1, 1};
-        vkc::ShaderManager gaussShaderMgr{pDeviceMgr, shader::gaussFilterV2SpirvCode};
+        vkc::ShaderManager gaussShaderMgr =
+            vkc::ShaderManager::create(pDeviceMgr, shader::gaussFilterV2SpirvCode) | unwrap;
         vkc::SpecConstantManager specConstantMgr{blockSize.x};
-        vkc::PipelineManager gaussPipelineMgr{pDeviceMgr, gaussPLayoutMgr, gaussShaderMgr,
-                                              specConstantMgr.getSpecInfo()};
+        vkc::PipelineManager gaussPipelineMgr =
+            vkc::PipelineManager::create(pDeviceMgr, gaussPLayoutMgr, gaussShaderMgr, specConstantMgr.getSpecInfo()) |
+            unwrap;
 
         gaussCmdBufMgr.begin();
         gaussCmdBufMgr.bindPipeline(gaussPipelineMgr);
